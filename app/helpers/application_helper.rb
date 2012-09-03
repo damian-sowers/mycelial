15e5473545ff1001e6c347f02b79fa32aa5d047a
Damian Sowers <damian.sowers@gmail.com>
2012-09-02 22:20:21 -0700

    new design, added sidebar and logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def controller?(*controller)
+    controller.include?(params[:controller])
+  end
+
+  def action?(*action)
+    action.include?(params[:action])
+  end
 end
