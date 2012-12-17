d98b9718ea5b10c2e777a2a7a9ad0574f208e78f
Damian Sowers <damian.sowers@gmail.com>
2012-12-17 10:21:13 -0800

    added default photos, bottom navbar

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,4 +3,19 @@ module PagesHelper
 		new_height = (original_height.to_f / original_width.to_f) * new_width
 		new_height.floor
 	end
+
+	def get_random_default(random_number)
+		case random_number
+			when 1
+				return "shrooms/orange-shroom.jpg"
+			when 2
+				return "shrooms/blue-shroom.jpg"
+			when 3
+				return "shrooms/red-shroom.jpg"
+			when 4
+				return "shrooms/white-shroom.jpg"
+			else
+				return "shrooms/orange-shroom.jpg"
+		end
+	end
 end
