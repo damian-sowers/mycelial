1c03bae2857e3489bd7425e97c05066f9764310c
Damian Sowers <damian.sowers@gmail.com>
2012-12-22 12:51:55 -0800

    timezone and non-precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module CNN
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Pacific Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -63,6 +63,9 @@ module CNN
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    #to satisfy heroku
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
