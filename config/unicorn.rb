15358349d3f7099ec63e51b6d2fe0c9cc2f67b79
Damian Sowers <damian.sowers@gmail.com>
2013-01-06 12:58:51 -0800

    add change to unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,10 @@
 worker_processes 4 # amount of unicorn workers to spin up
-timeout 50         # restarts workers that hang for 30 seconds
+timeout 40         # restarts workers that hang for 30 seconds
 preload_app true # need this for newrelic
+
+#trying to get rid of pgerror:EOF detected. http://stackoverflow.com/questions/8497039/on-heroku-cedar-with-unicorn-getting-activerecordstatementinvalid-pgerror
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection 
+  end
+end
