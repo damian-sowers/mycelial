489222bb7572f7c0049dcd8cfff78ebc0cca6389
Damian Sowers <damian.sowers@gmail.com>
2013-04-10 14:23:37 -0700

    fix tab size

--- app/controllers/beta_invites_controller.rb
+++ app/controllers/beta_invites_controller.rb
@@ -1,19 +1,19 @@
 class BetaInvitesController < ApplicationController
 
-	def new
-		@beta_invite = BetaInvite.new
-	end
+  def new
+    @beta_invite = BetaInvite.new
+  end
 
-	def create
-		@beta_invite = BetaInvite.new(params[:beta_invite])
-		if @beta_invite.save
-			flash[:success] = "Thanks for signing up."
-			redirect_to :controller => "beta_invites", :action => "thanks", only_path: true
-		else
+  def create
+    @beta_invite = BetaInvite.new(params[:beta_invite])
+    if @beta_invite.save
+      flash[:success] = "Thanks for signing up."
+      redirect_to :controller => "beta_invites", :action => "thanks", only_path: true
+    else
 
-		end
-	end
+    end
+  end
 
-	def thanks
-	end
+  def thanks
+  end
 end
