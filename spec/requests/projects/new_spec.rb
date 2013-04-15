6c3faf971e7ed6adaf9d6b1a00f4cc6dbc50671a
Damian Sowers <damian.sowers@gmail.com>
2013-04-14 21:08:02 -0700

    new project spec

---
+++ spec/requests/projects/new_spec.rb
@@ -0,0 +1,46 @@
+require "spec_helper.rb"
+
+describe "new project" do
+  let(:user) { FactoryGirl.create(:user) }
+  subject { page }
+  before do
+    sign_in(user)
+  end
+
+  describe "user clicks 'add a new project'" do
+    before do 
+      visit edit_page_path(user)
+      click_link "Add a new project"
+    end
+    
+    it { should have_selector('a', :text => "Programming Project") }
+    it { should have_selector('a', :text => "Blog Post or Article") }
+    it { should have_selector('a', :text => "Other") }
+
+    it "should redirec to to new project form" do
+      click_link "Programming Project"
+      should have_content("Add a new project to your page") 
+    end
+
+    describe "user fills in new project form with valid information" do
+      before do
+        visit new_project_path(:project_type => 1)
+        fill_in "Project name", with: "Test Project"
+        fill_in "Short description", with: "A little Text"
+        fill_in "Long Description", with: "More text"
+        # fill_in "Main Languages and Technologies (e.g Ruby, Rails, Python)", with: "rails"
+        fill_in "Other interesting things about this project", with: "some stuff"
+        fill_in "Project URL", with: "http://www.google.com"
+        fill_in "Github Repo", with: "http://www.github.com/damian-sowers/mycelial"
+        # attach_file 'Upload a Profile Picture', "#{Rails.root}/spec/fixtures/images/code.png"
+        click_button "Submit"
+      end
+
+      it "should redirect user to show page" do
+        current_path.should == page_path(user.username)
+      end
+      it { should have_content("Projects") } 
+      it { should have_content("Test Project") } 
+    end
+  end
+end
