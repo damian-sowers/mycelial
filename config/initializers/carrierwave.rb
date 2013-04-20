a37283d8153d28ac9e073f3b7ea96b74e2697470
Harper Henn <harper.henn@gmail.com>
2013-04-20 19:23:56 -0400

    Add specs for models

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,20 @@
 CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp')
   config.cache_dir = 'carrierwave'
-  config.fog_credentials = {
-    :provider               => 'AWS',       # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],       # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
-    :region                 => 'us-west-1'  # optional, defaults to 'us-east-1'
-  }
-  config.fog_directory  = 'mycelial-images'                     # required
-  #config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
-  config.fog_public     = true                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  if Rails.env.test? or Rails.env.cucumber?
+    # File storage if in test environment
+    config.storage = :file
+  else
+    config.fog_credentials = {
+      :provider               => 'AWS',       # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],       # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],       # required
+      :region                 => 'us-west-1'  # optional, defaults to 'us-east-1'
+    }
+    config.fog_directory  = 'mycelial-images'                     # required
+    #config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
+    config.fog_public     = true                                   # optional, defaults to true
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  end
 end
 #testing this out.
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,7 +1,28 @@
 require 'spec_helper'
 
 describe Notification do
-  
+  let(:notification) { create(:notification) }
+  let(:sender) { create(:user) }
+  let(:receiver) { create(:user, username: 'other', email: 'other@other.com') }
+  let(:page) { create(:page, user_id: receiver.id) }
+  let(:project) { create(:project, page_id: page.id) }
+
+  it { should respond_to :sender_id }
+  it { should respond_to :receiver_id }
+  it { should respond_to :notification_type }
+  it { should respond_to :notification_id }
+
+  describe "::get_new_notifications" do
+    before do
+      # Notification creation triggered after comment/like creation
+      Comment.create(project_id: project.id, user_id: sender.id)
+    end
+
+    it "returns unread notifications for a user" do
+    notification = Notification.get_new_notifications(receiver.id)[0]
+      notification.viewed.should eq 0
+    end
+  end
 end
 # == Schema Information
 #
