cf11df30e30b177c8af513db1ab103127d825858
Harper Henn <harper.henn@gmail.com>
2013-04-28 10:52:50 -0400

    Add specs for TechTag/Page models, Welcome/Notification mailers

--- spec/mailers/welcome_spec.rb
+++ spec/mailers/welcome_spec.rb
@@ -1,5 +1,20 @@
 require "spec_helper"
 
 describe Welcome do
-  
+
+  let(:user) { create :user }
+  let(:sent) { ActionMailer::Base.deliveries }
+
+  describe "#send_welcome_email" do
+    before { Welcome.send_welcome_email(user.email).deliver }
+
+    it "delivers mail" do
+      sent.should_not be_empty
+    end
+
+    it "sends the correct message" do
+      email = sent.last
+      email.subject.should eq "Damian from Mycelial. Thanks for trying it out!"
+    end
+  end
 end
--- spec/models/tech_tag_spec.rb
+++ spec/models/tech_tag_spec.rb
@@ -2,7 +2,53 @@ require 'spec_helper'
 
 describe TechTag do
   
+  let(:tech_tag) { create(:tech_tag) }
+
+  subject { tech_tag }
+
+  it { should respond_to :name }
+  it { should respond_to :picture }
+
+  describe "::tokens" do
+    context "when TechTags with a matching name are found" do
+      before { create(:tech_tag) }
+
+      it "returns matched TechTags" do
+        query = TechTag.tokens("rails")
+        query[0].name.should eq "rails"
+      end
+    end
+
+    context "when matching TechTags are not found" do
+      let(:bad_query) { TechTag.tokens("python") }
+
+      it "returns an :id" do
+        bad_query[0][:id].should eq "<<<python>>>"
+      end
+
+      it "returns a :name" do
+        bad_query[0][:name].should eq "New: \"python\""
+      end
+    end
+  end
+
+  describe "::ids_from_tokens" do
+    before { @ids = TechTag.ids_from_tokens("<<<ruby>>>") }
+
+    it "creates TechTags from tokens" do
+      TechTag.count.should eq 1
+    end
+
+    it "assigns TechTag names from tokens" do
+      TechTag.first.name.should eq "ruby"
+    end
+
+    it "returns an array of TechTag ids" do
+      @ids.should eq ["1"]
+    end
+  end
 end
+
 # == Schema Information
 #
 # Table name: tech_tags
